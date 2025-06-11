da1998a987b2bb57f3af9032f57bb0f55570a5a5
jujuReggio <juli.reggio@gmail.com>
2025-06-11 11:48:33 -0300

    Post Process script fixed

--- Assets/shader and materials/Post Processing/PostProcessing.cs
+++ Assets/shader and materials/Post Processing/PostProcessing.cs
@@ -6,16 +6,20 @@ using TMPro;
 public class PostProcessing : MonoBehaviour
 {
     [Header("Referencias")]
-    [SerializeField] private EffectRenderer effectRenderer; // arrastra la cámara con el script
+    [SerializeField] private EffectRenderer effectRenderer;
     [SerializeField] private TMP_Dropdown dropdown;
 
     [Header("Materials")]
     [SerializeField] private Material grayScaleMaterial;
     [SerializeField] private Material pixelMaterial;
+    [SerializeField] private Material vignetteMaterial;
 
     [Header("Sliders")]
     [SerializeField] private Slider graySlider;
     [SerializeField] private Slider pixelSlider;
+    [SerializeField] private Slider vignettePowerSlider; 
+    [SerializeField] private Slider vignetteHardnessSlider;
+    [SerializeField] private Slider vignetteScaleSlider;
 
     [Header("Panels")]
     [SerializeField] private GameObject grayPanel;
@@ -25,13 +29,20 @@ public class PostProcessing : MonoBehaviour
     private void Start()
     {
         dropdown.onValueChanged.AddListener(OnEffectChanged);
-        OnEffectChanged(dropdown.value); // aplicar el actual al iniciar
+        OnEffectChanged(dropdown.value);
 
         if (graySlider != null)
             graySlider.onValueChanged.AddListener(val => grayScaleMaterial.SetFloat("_intensity", val));
 
         if (pixelSlider != null)
             pixelSlider.onValueChanged.AddListener(val => pixelMaterial.SetFloat("_Resolution", val));
+
+        if (vignettePowerSlider != null)
+            vignettePowerSlider.onValueChanged.AddListener(val => vignetteMaterial.SetFloat("_MaskPower", val));
+        if (vignetteHardnessSlider != null)
+            vignetteHardnessSlider.onValueChanged.AddListener(val => vignetteMaterial.SetFloat("_MaskHardness", val));
+        if (vignetteScaleSlider != null)
+            vignetteScaleSlider.onValueChanged.AddListener(val => vignetteMaterial.SetFloat("_MaskScale", val));
     }
 
     private void OnEffectChanged(int index)
@@ -51,7 +62,7 @@ public class PostProcessing : MonoBehaviour
                 pixelPanel.SetActive(true);
                 break;
             case 2: // Vignette
-                effectRenderer.currentEffectMaterial = null;
+                effectRenderer.currentEffectMaterial = vignetteMaterial;
                 vignettePanel.SetActive(true);
                 break;
             default:
